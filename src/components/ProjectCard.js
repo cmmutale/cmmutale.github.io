1fa4e886385243936eb82e548b35e42bf3937024
Chilufya <cmutalem@gmail.com>
2023-12-21 17:27:00 -0600

    update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,32 +5,17 @@ import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 
 function ProjectCard(props) {
     return (
-        <div className='flex items-center gap-4 bg-white/10 bg--blur sm:px-4 px-2 py-2 rounded-md w-full'>
-            {/* <div className='aspect-video basis-1/6'>
-                <img src='/images/hostshare.png' />
-            </div> */}
-            <div className='grow space-y-4'>
-                <div className='text-2xl my-1 flex justify-between items-center'>
-                    <h2>{props.title}</h2>
-                    <Link 
-                    to={props.link} 
-                    target='_blank'
-                    className='hover:translate-x-2 duration-300 transition-all'><FaArrowRightLong /></Link>
-                </div>
+        <div className='space-y-4 mb-8'>
+            <div>
+                <h1 className='text-[1.75rem] font-medium'>{props.title}</h1>
                 <p>{props.desc}</p>
-                <div className='tag-list flex items-center gap-2 bg-white/10 w-fit px-1 rounded-md'>
-                    {
-                        props.tags.map((item, index) => {
-                            return <>
-                                <div className='flex items-center gap-2'>
-                                    {item}
-                                </div>
-                                <span className='block w-1 aspect-square bg-white rounded-full'></span>
-                            </>
-                        })
-                    }
-                </div>
             </div>
+            <Link
+                to={props.link}
+                target='_blank'
+                className='block'>
+                <button className='btn bg-white text-black'>Open</button>
+            </Link>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,39 +5,22 @@ import ProfileCard from "../components/ProfileCard"
 import Navigation from "../components/Navigation"
 import Projects from "../components/sections/Projects"
 import Resume from "../components/sections/Resume"
+import Layout from "../components/layout"
 
 
 
 const IndexPage = () => {
     return (
-        <main className="px-2 scroll-smooth">
-            <Header />
-            <div className="min-h-screen flex justify-center items-center p-8">
-                <section className="container mx-auto">
-                    <div className="flex md:flex-row flex-col gap-8 items-center">
-                        <div className="lg:basis-1/3 basis-1/2 shrink-0 space-y-4 sticky top-0">
-                            <ProfileCard />
-                            <span className="block h-[2px] w-full bg-white/10 shadow-lg"></span>
-                            <Navigation />
-                        </div>
-                        <div className="grow">
-                        </div>
-                    </div>
-                </section>
-            </div>
-            <section id="projects" className="min-h-screen flex justify-center items-center p-8">
-                <div className="container mx-auto">
-                    <h1 className="text-2xl underline my-8">Portfolio</h1>
-                    <Projects />
-                </div>
+        <Layout>
+            <section className="pt-8">
+                <ProfileCard />
             </section>
-            <section id="resume" className="min-h-screen flex justify-center items-center p-8">
-                <div className="container mx-auto">
-                    <h1 className="text-2xl underline my-8">Work History</h1>
-                    <Resume />
-                </div>
+            <section className="space-y-4">
+                <h1>where I've been</h1>
+                <Resume />
             </section>
-        </main>
+        </Layout>
+
     )
 }
 
