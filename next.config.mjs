328094dd280193f73983f3f86b27ee8a00e2d78d
Chilufya Mutale <cmutalem@gmail.com>
2024-07-15 14:37:38 -0500

    updated to nextjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export", // export the app as an static site
+    basePath: "/"
+    // images: {
+    //     unoptimized: true, //  disable optimization for static site
+    // },
+    // reactStrictMode: true, //  enable strict mode for React
+};
+
+export default nextConfig;
