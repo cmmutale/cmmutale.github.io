76fc4083b5b8a7a70d73c12b8ba140f125cffb3b
Chilufya Mutale <cmutalem@gmail.com>
2024-12-19 15:15:02 -0600

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cmmutale.github.io',
-    base: 'cmmutale',
+  site: 'https://cmmutale.github.io',
+  base: 'cmmutale',
+  integrations: [tailwind()]
 });
