29a9e3e22e9dd02e68eb03e65b4e36aeebd35740
Chilu <cmutalem@gmail.com>
2025-01-27 18:17:35 -0600

    adding tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cmmutale.github.io',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+		// optimizeDeps: {
+		// 	exclude: ['@resvg/resvg-js']
+		// }
+	}
 });
