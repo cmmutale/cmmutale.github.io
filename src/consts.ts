4a5884e1a239ffcaef44bd97e611fb03f820d4ba
Chilu <cmutalem@gmail.com>
2025-03-23 13:33:41 -0500

    improving seo

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Chilu\'s blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Custom UI/UX Developer for Websites and Software';
+export const SITE_DESCRIPTION = 'Get custom built websites and software to help your business and startup get more customers.';
 export const SITE_REPO = 'https://github.com/cmmutale/cmmutale.github.io'
